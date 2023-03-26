5204715fd2bff84cc3f276dabe63e8b721ba933b
joafjell <jafjell@hiof.no>
2023-03-26 16:45:22 +0200

    mer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 import './css/main.css'
 
 function App() {
+
+  const [movies, setMovies] = useState([])
+  const [search, setSearch] = useState('James Bond')
+
+  const getMovies = async() =>{
+    const response = await fetch(`http://www.omdbapi.com/search?q=${search}?i=tt3896198&apikey=265fbd2f`)
+    const movieInformation = await response.json()
+    setMovies(movieInformation.hits)
+  }
+
+  useEffect(() => {
+    getMovies()
+  }, [])
+
   return (
     <h1>Filmsøk</h1>
   );
