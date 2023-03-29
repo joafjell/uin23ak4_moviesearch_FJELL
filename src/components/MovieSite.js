7ca35e8201128adad87fa8b985a23fbc58990828
joafjell <jafjell@hiof.no>
2023-03-29 17:23:03 +0200

    forsøk for å få brukt film informasjonen

--- src/components/MovieSite.js
+++ src/components/MovieSite.js
@@ -1,21 +1,33 @@
+//import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 export default function MovieSite({movies}){
     const {slug} = useParams()
     const movie = movies.find((m) => m?.movie?.label.replace(/\s/g, "-").toLowercase() === slug)
-    
+ 
+
     fetch(`http://www.omdbapi.com/?apikey=e3f58a23&i=${slug}`)
         .then(response => response.json())
-        .then(data => console.log(data, "2"))
-
+        .then(movieInfo => console.log(movieInfo, "2"))
     
-        
+ 
+    
+    /*const getMovieInfo = async() => {
+        const response = await fetch(`http://www.omdbapi.com/?apikey=e3f58a23&i=${slug}`)
+        const movieInf = await response.json()
+        setMovieInfo(movieInf.hits)
+    }
+    useEffect(() => {
+        getMovieInfo()
+    },[])
+
+    console.log(movieInfo, "3")*/
 
     return(
         <section>
-            
-            <img src={movie?.movie.poster} alt={movie?.movie.title}/>
-            <p>Skuespillere: </p>
+            <h2></h2>
+            <img src={slug?.Poster} alt={slug?.Title}/>
+            <p>Skuespillere: {slug?.Actors} </p>
             <ul>
 
             </ul>
