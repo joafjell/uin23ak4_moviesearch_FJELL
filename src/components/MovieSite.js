d6df3c722d8389888d9652b7ce92f78e225841d5
muontian <89585394+muontian@users.noreply.github.com>
2023-03-29 16:21:36 +0200

    Update MovieSite.js

--- src/components/MovieSite.js
+++ src/components/MovieSite.js
@@ -4,10 +4,19 @@ export default function MovieSite({movies}){
     const {slug} = useParams()
     const movie = movies.find((m) => m?.movie?.label.replace(/\s/g, "-").toLowercase() === slug)
     
+    fetch(`http://www.omdbapi.com/?apikey=e3f58a23&i=${slug}`)
+        .then(response => response.json())
+        .then(data => console.log(data, "2"))
+
+    const data = {
+        Actors: "Guppi, Hindi, Suri"
+    }
+
     return(
         <section>
-            <h1>{movie?.movie.label}</h1>
-            <img src={movie?.movie.poster} alt={movie?.movie.label}/>
+            <h2>{movie?.movie?.title}</h2>
+            <img src={movie?.movie.poster} alt={movie?.movie.title}/>
+            <p>Skuespillere: </p>
             <ul>
 
             </ul>
