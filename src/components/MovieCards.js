095158a06420d1c32f4378c3e180af309eeee6c7
joafjell <jafjell@hiof.no>
2023-03-29 19:53:49 +0200

    update, fikset mer CSS
    gjort siden responsiv

--- src/components/MovieCards.js
+++ src/components/MovieCards.js
@@ -8,6 +8,7 @@ export default function MovieCards({title, img, type, year, imdbID}){
             </Link>
             <p>Type: <span>{type}</span></p>
             <p>Utgivelses år: <span>{year}</span></p>
+            <Link to={imdbID}><button className="more-info-button">Click for more info</button></Link>
             
         </article>
 
